a2595b35e7ec04dec712398ac878755b69ff1c3d
Michele Balistreri <michele@bitgamma.com>
2022-11-25 10:03:55 +0100

    constant time comparison

--- src/main/java/im/status/keycard/Crypto.java
+++ src/main/java/im/status/keycard/Crypto.java
@@ -89,11 +89,7 @@ public class Crypto {
 
     addm256(output, outOff, data, dataOff, SECP256k1.SECP256K1_R, (short) 0, output, outOff);
 
-    if (isZero256(output, outOff)) {
-      return false;
-    }
-
-    return true;
+    return !isZero256(output, outOff);
   }
 
   /**
@@ -202,17 +198,22 @@ public class Crypto {
    * @return the comparison result
    */
   private short ucmp256(byte[] a, short aOff, byte[] b, short bOff) {
-    short ai, bi;
+    short gt = 0;
+    short eq = 1;
+    
     for (short i = 0 ; i < 32; i++) {
-      ai = (short)(a[(short)(aOff + i)] & 0x00ff);
-      bi = (short)(b[(short)(bOff + i)] & 0x00ff);
+      short l = (short)(a[(short)(aOff + i)] & 0x00ff);
+      short r = (short)(b[(short)(bOff + i)] & 0x00ff);
+      short d = (short)(r - l);
+      short l_xor_r = (short)(l ^ r);
+      short l_xor_d = (short)(l ^ d);
+      short d_xored = (short)(d ^ (short)(l_xor_r & l_xor_d));
 
-      if (ai != bi) {
-        return (short)(ai - bi);
-      }
+      gt |= (d_xored >>> 15) & eq;
+      eq &= ((short)(l_xor_r - 1) >>> 15);
     }
 
-    return 0;
+    return (short) ((gt + gt + eq) - 1);
   }
 
   /**
@@ -223,16 +224,13 @@ public class Crypto {
    * @return true if a is 0, false otherwise
    */
   private boolean isZero256(byte[] a, short aOff) {
-    boolean isZero = true;
+    byte acc = 0;
 
-    for (short i = 0; i < (byte) 32; i++) {
-      if (a[(short)(aOff + i)] != 0) {
-        isZero = false;
-        break;
-      }
+    for (short i = 0; i < 32; i++) {
+      acc |= a[(short)(aOff + i)];
     }
 
-    return isZero;
+    return acc == 0;
   }
 
   /**
